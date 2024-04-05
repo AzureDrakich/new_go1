16bf96c2a48fdd849244352068bdc0c79bdeb32f
_ <-@>
2024-04-06 01:01:43 +0300

    1

---
+++ internal/app/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+	"log"
+	"net/http"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "postgres"
+)
+
+func main() {
+	psql := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psql)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully Conected")
+	fmt.Println("HTTP SERVER started at localhost:8080")
+	router := mux.NewRouter().StrictSlash(true)
+	log.Fatal(http.ListenAndServe("localhost:8080", router))
+}
